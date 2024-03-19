d80708c9a5810d954ed8e53187f82762b889de0f
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-03-19 11:51:29 +0100

    move navigation drawer to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    <NavigationDrawer :drawer="drawer" :items="items" />
+    
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
-
+    
     <v-main>
       <router-view></router-view>
     </v-main>
@@ -49,12 +14,18 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      drawer: null,
-      items: [
-          { title: 'Albums', icon: 'mdi-album', to: '/albums' },
-          { title: 'Artists', icon: 'mdi-account-music', to: '/artists' },
-        ],}),
-  }
+import NavigationDrawer from './components/NavigationDrawer.vue';
+
+export default {
+  components: {
+    NavigationDrawer,
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Albums', icon: 'mdi-album', to: '/albums' },
+      { title: 'Artists', icon: 'mdi-account-music', to: '/artists' },
+    ],
+  }),
+};
 </script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    app>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6">
+          Application
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          subtext
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list
+      dense
+      nav
+    >
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :to="item.to"
+        link
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Albums', icon: 'mdi-album', to: '/albums' },
+      { title: 'Artists', icon: 'mdi-account-music', to: '/artists' },
+    ],
+  }),
+};
+</script>
