eeec3674315a969508c3d56ca5c1ebbc10d2df0a
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-03-20 13:05:57 +0100

    successfully retreive from pokeapi

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="pokemon pa-8">
-    <h1>Under construction</h1>
-  </div>
+    <div class="pokemon pa-8">
+    </div>
 </template>
 
+<style scoped>
+.pokemon-grid {
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
+
 <script>
-  export default {
-    name: 'Pokémon',
+import axios from 'axios';
+
+export default {
+    name: 'Pokemon',
+    data() {
+        return {
+            pokemon: {
+                name: "",
+                imgurl: ""
+            },
+            pokedex: []
+        };
+    },
+    created() {
+        let instance = this;
+        let LAST_PKMN = 1025
+        for (let i = 1; i <= LAST_PKMN; i++) {
+            axios
+                .get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                .then(response => {
+                    let pokemon = {
+                        name: response.data.name,
+                        imgurl: response.data.sprites.front_default
+                    }
+                    instance.pokedex.push(pokemon);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    },
     mounted() {
         this.$emit('updateTitle', this.$options.name);
     }
-  }
-
+};
 </script>
